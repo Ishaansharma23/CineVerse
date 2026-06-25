021b650123af843d2b5f1b9cca0da8a209dc87b9
Ishaan Sharma <sharmaishaan0009@gmail.com>
2026-06-25 12:35:02 +0530

    made the schema for the threatre model

---
+++ backend/models/Threatre.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const theatreSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Theatre name is required'],
+      trim: true, // String ke starting aur ending ke extra spaces hata do.
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // acha wo threatre k owner ko refer kr rha user collection mai 
+      required: true,
+    },
+
+    city: {
+      type: String,
+      required: [true, 'City is required'],
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      required: [true, 'Address is required'],
+      trim: true,
+    },
+
+    amenities: [ // Theatre me available facilities.
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    screens: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Screen',
+      },
+    ],
+
+    status: {
+      type: String,
+      enum: ['pending', 'approved', 'rejected'], // Ye validation hai.
+      default: 'pending',
+    },
+
+    isActive: { // active threatres milenge
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Theatre', theatreSchema);
